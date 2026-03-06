247d038f67bb9c84e614cf1e959029c7a5b882be
Sundar Rajan M <sundarrajan0710@gmail.com>
2026-03-07 02:19:15 +0530

    Update 01_cohort_analysis.sql

--- Scripts/01_cohort_analysis.sql
+++ Scripts/01_cohort_analysis.sql
@@ -1,18 +1,35 @@
+/*
+Project: Customer Retention Analytics (SQL Portfolio)
+Script: 01_cohort_analysis.sql
+Goal: Identify the first purchase month (cohort) for each customer.
+
+Description:
+This query determines when each customer made their first purchase
+and assigns them to a cohort based on that purchase month.
+The cohort will later be used for retention analysis.
+*/
+
 WITH first_purchase AS (
 SELECT
     CustomerKey,
-    MIN(OrderDate) AS first_purchase_date   -- find the first purchase date for each customer
+    MIN(OrderDate) AS first_purchase_date
 FROM FactInternetSales
-GROUP BY
-    CustomerKey
-)
+GROUP BY CustomerKey
+)  -- Find the first purchase date for each customer
+
+-- Assign each customer to a cohort month and retrieve customer details
 SELECT
     fp.CustomerKey,
-    dc.FirstName + ' ' + dc.LastName AS customer_name,   -- get customer full name from dimension table
-    DATEFROMPARTS(YEAR(fp.first_purchase_date),MONTH(fp.first_purchase_date),1) AS cohort_month   -- convert first purchase date to cohort month
+    dc.FirstName + ' ' + dc.LastName AS customer_name,
+    DATEFROMPARTS(
+        YEAR(fp.first_purchase_date),
+        MONTH(fp.first_purchase_date),
+        1
+    ) AS cohort_month
 FROM first_purchase fp
 JOIN DimCustomer dc
-    ON fp.CustomerKey = dc.CustomerKey   -- join to get customer details
+    ON fp.CustomerKey = dc.CustomerKey
 ORDER BY
     cohort_month,
-    fp.CustomerKey;   -- sort customers by cohort month
+    fp.CustomerKey;
+
